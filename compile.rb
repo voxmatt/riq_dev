eac8d1073bdd8cb90b9b061eb6aa2281364bef4c
Jason Li <jason@relateiq.com>
2013-12-23 01:06:11 -0800

    change to use JSON library

--- compile.rb
+++ compile.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'json'
+
+
 ##############
 ## SETTINGS ##
 ##############
@@ -34,10 +38,68 @@ $api_text = '{'
 def directory_depth(file, startDir)
 
   relPath = File.expand_path(file).split(startDir)[1]
-  depth = relPath.split("/").length - 1
+  depth = relPath.split("/").length
   return depth
 
 end
+
+def build_api_map(depth, currdir)
+  Dir.chdir currdir
+
+  #depth = directory_depth(currdir, topdir)
+  map = {}
+  
+  if depth > 0
+    map[:title] = File.basename(currdir).gsub(/^\d\d-/, '').gsub('-', ' ')
+    map[:directoryPath] = File.expand_path(currdir)
+  end
+
+  
+  print "depth:",depth," currdir:",currdir,"\n"
+
+  map[:superSections] = [] if depth == 0
+  map[:sections] = [] if depth == 1
+  map[:subSections] = [] if depth == 2
+  Dir.glob("*").each do |filename|
+
+    print "file:",filename,"\n"
+
+    # If it's a directory, it signals a new section
+    # so we need particular keys in the JSON file
+    if File.directory? filename
+      puts "directory"
+      newdepth = depth + 1
+      map[:superSections].push(build_api_map(newdepth, filename)) if depth == 0
+      map[:sections].push(build_api_map(newdepth, filename))  if depth == 1
+      map[:subSections].push(build_api_map(newdepth, filename))  if depth == 2
+
+    # Now we check if the file is a description file
+    elsif File.basename(filename).split(".")[0] == "description"
+      puts "desc"
+      map[:description] = File.expand_path(filename).split($docDir)[1]
+
+    # Next we look at the example files
+    elsif File.basename(filename).split(".")[0] == "example"
+      puts "example"
+
+      fileExt = filename.split(".")[1]
+
+      $lang_key = ""
+      $languages.each do |lang|
+        $lang_key = lang[0] if fileExt == lang[1]
+      end
+
+      map[$lang_key] = File.expand_path(filename).split($docDir)[1]
+
+    end
+
+  end
+  Dir.chdir "../"
+  
+  return map
+end
+
+
 
 # This is the meet and potatoes of the script
 def add_api_info(directory)
@@ -110,10 +172,12 @@ def add_api_info(directory)
 end
 
 # Finally, we call the function to initiate the JSON compile
-add_api_info($docDir)
+#add_api_info($docDir)
+$api_hash = build_api_map(0, $docDir)
 
 # Then we move back to the root directory and write the JSON file
 Dir.chdir $rootDir
 File.open("api.json", "w+") do |file|
-  file.write($api_text)
+  #file.write($api_text)
+  file.write(JSON.pretty_generate($api_hash))
 end
